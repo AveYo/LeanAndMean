fc81a95c6011020dc51eff25ed3295595b9f046e
AveYo <aveyo@yahoo.com>
2024-10-23 02:17:45 +0300

    Update fix_network_bufferbloat.bat
    adjusted QoS (more fairness) and TCP congestion rules (less aggressive)

--- fix_network_bufferbloat.bat
+++ fix_network_bufferbloat.bat
@@ -1,4 +1,4 @@
-@(set ^ "f0=%temp%\FixNetworkBufferbloat.ps1" -desc ')|| AveYo, 2024.10.17
+@(set ^ "f0=%temp%\FixNetworkBufferbloat.ps1" -desc ')|| AveYo, 2024.10.20
 @(fc %0 "%f0%" 2>&1||copy /b %0+nul "%f0%" /y)>nul& powershell -nop -ep RemoteSigned -f "%f0%" %* -dp0 "%CD%"
 @exit /b '); . { Param($dp0 = $pwd.Path); $dp0 = $dp0.Trim('" \'); $n0 = ${^}-replace'^.+\\|.{4}$',''; cd -l "$dp0\" -ea 0
 
@@ -12,6 +12,8 @@ write-host @'
   Download Autotuning OFF - disables TCP Window Scaling, hurting gigabit speeds 
 
 '@
+
+#:: 2024.10.20 adjusted QoS and TCP congestion rules
 
 #:: Args / Dialog - can use commandline parameters to skip the prompt
 $cl = @{2 = 'both'; 1 = 'upload'; 0 = 'download'; -1 = 'reset'}
@@ -41,7 +43,7 @@ foreach ($a in Get-NetAdapter -Physical | Select-Object DeviceID,Name) {
 $NICs = $NIC.Keys -join ', '
 
 if ($do -eq 'upload' -or $do -eq 'both') {
-  $RWSCALING = 'Normal'; $QoS = 50; $MBW = $QoS - 1
+  $RWSCALING = 'Normal'; $QoS = 80; $MBW = 25
   write-host " Upload QoS ON" -fore Yellow; . {
     ni "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\QoS" -ea 0
     sp "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\QoS" "Do not use NLA" 1 -type string -force -ea 0
@@ -88,7 +90,7 @@ if ($do -ne 'reset') {
     sp "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" MaximumReassemblyHeaders 0xffff -type dword -force -ea 0 # 0
     sp "HKLM:\SYSTEM\CurrentControlSet\Services\AFD\Parameters" FastSendDatagramThreshold 1500 -type dword -force -ea 0
     sp "HKLM:\SYSTEM\CurrentControlSet\Services\AFD\Parameters" DefaultReceiveWindow $(2048 * 4096) -type dword -force -ea 0
-    sp "HKLM:\SYSTEM\CurrentControlSet\Services\AFD\Parameters" DefaultSendWindow $(4096 * 4096) -type dword -force -ea 0
+    sp "HKLM:\SYSTEM\CurrentControlSet\Services\AFD\Parameters" DefaultSendWindow $(2048 * 4096) -type dword -force -ea 0
   }
 
   " temporarily disable $NICs"; . { $NIC.Keys | foreach { Disable-NetAdapter -InterfaceAlias "$_" -Confirm:$False } }
@@ -100,10 +102,10 @@ if ($do -ne 'reset') {
   # restore custom mac
     if ($mac) { Set-NetAdapterAdvancedProperty -Name "$_" -RegistryKeyword "NetworkAddress" -RegistryValue $mac }
   # set receive and transmit buffers - less is better for latency, worst for throughput; too less and packet loss increases
-    $rx = (Get-NetAdapterAdvancedProperty -Name "$_" -RegistryKeyword "*ReceiveBuffers").NumericParameterMaxValue  # $rx 1024 320
-    $tx = (Get-NetAdapterAdvancedProperty -Name "$_" -RegistryKeyword "*TransmitBuffers").NumericParameterMaxValue # $tx 2048 160
-    Set-NetAdapterAdvancedProperty -Name "$_" -RegistryKeyword "*ReceiveBuffers"  -RegistryValue $([math]::min($rx,1024))
-    Set-NetAdapterAdvancedProperty -Name "$_" -RegistryKeyword "*TransmitBuffers" -RegistryValue $([math]::min($tx,2048))
+    $rx = (Get-NetAdapterAdvancedProperty -Name "$_" -RegistryKeyword "*ReceiveBuffers").NumericParameterMaxValue  
+    $tx = (Get-NetAdapterAdvancedProperty -Name "$_" -RegistryKeyword "*TransmitBuffers").NumericParameterMaxValue
+    Set-NetAdapterAdvancedProperty -Name "$_" -RegistryKeyword "*ReceiveBuffers"  -RegistryValue $rx # $rx 1024 320
+    Set-NetAdapterAdvancedProperty -Name "$_" -RegistryKeyword "*TransmitBuffers" -RegistryValue $tx # $tx 2048 160
   # pci-e adapters in msi-x mode from intel are generally fine with ITR Adaptive - others? not so much
     Set-NetAdapterAdvancedProperty -Name "$_" -RegistryKeyword "*InterruptModeration" -RegistryValue 0 # Off 0 On 1
     Set-NetAdapterAdvancedProperty -Name "$_" -RegistryKeyword "ITR" -RegistryValue 0 # Off 0 Adaptive 65535
@@ -113,7 +115,7 @@ if ($do -ne 'reset') {
   # priority tag
     Set-NetAdapterAdvancedProperty -Name "$_" -RegistryKeyword "*PriorityVLANTag" -RegistryValue 1
   # undesirable stuff 
-    Set-NetAdapterAdvancedProperty -Name "$_" -RegistryKeyword "*FlowControl" -RegistryValue 3
+    Set-NetAdapterAdvancedProperty -Name "$_" -RegistryKeyword "*FlowControl" -RegistryValue 0
     Set-NetAdapterAdvancedProperty -Name "$_" -RegistryKeyword "*JumboPacket" -RegistryValue 1514
     Set-NetAdapterAdvancedProperty -Name "$_" -RegistryKeyword "*HeaderDataSplit" -RegistryValue 0
     Set-NetAdapterAdvancedProperty -Name "$_" -RegistryKeyword "TcpSegmentation" -RegistryValue 0
@@ -128,38 +130,36 @@ if ($do -ne 'reset') {
   } } 2>'' 1>''
 
   " Set-NetOffloadGlobalSetting"; . {
-    Set-NetOffloadGlobalSetting -TaskOffload Enabled # Disabled
+    Set-NetOffloadGlobalSetting -TaskOffload Enabled
     Set-NetOffloadGlobalSetting -Chimney Disabled
     Set-NetOffloadGlobalSetting -PacketCoalescingFilter Disabled
     Set-NetOffloadGlobalSetting -ReceiveSegmentCoalescing Disabled
     Set-NetOffloadGlobalSetting -ReceiveSideScaling Enabled
     Set-NetOffloadGlobalSetting -NetworkDirect Enabled
-    Set-NetOffloadGlobalSetting -NetworkDirectAcrossIPSubnets Allowed -ea 0 # Blocked
-  } #2>'' 1>''
+    Set-NetOffloadGlobalSetting -NetworkDirectAcrossIPSubnets Allowed -ea 0
+  } 2>'' 1>''
 
   " Enable-NetAdapterRss"; . { $NIC.Keys |foreach {
-    Set-NetAdapterRss -Name "$_" -NumberOfReceiveQueues 2 -BaseProcessorNumber 2 -MaxProcessorNumber 4 -Profile $(
-      "NUMAStatic") -Enabled $true -ea 0
+    Set-NetAdapterRss -Name "$_" -NumberOfReceiveQueues 2 -MaxProcessorNumber 4 -Profile "NUMAStatic" -Enabled $true -ea 0
     Enable-NetAdapterQos -Name "$_" -ea 0
     Enable-NetAdapterChecksumOffload -Name "$_" -ea 0
-
     Disable-NetAdapterRsc -Name "$_" -ea 0
     Disable-NetAdapterUso -Name "$_" -ea 0
     Disable-NetAdapterLso -Name "$_" -ea 0
     Disable-NetAdapterIPsecOffload -Name "$_" -ea 0
     Disable-NetAdapterEncapsulatedPacketTaskOffload -Name "$_" -ea 0
-  } } #2>'' 1>''
+  } } 2>'' 1>''
 
   " enable $NICs"; . { $NIC.Keys | foreach { Enable-NetAdapter -InterfaceAlias "$_" -Confirm:$False } }
 
   " netsh tweaks"; . {
     netsh winsock set autotuning on                                    # Winsock send autotuning
     netsh int udp set global uro=disabled                              # UDP Receive Segment Coalescing Offload - 11 24H2
-    netsh int tcp set heuristics wsh=disabled forcews=disabled          # Window Scaling heuristics
+    netsh int tcp set heuristics wsh=disabled forcews=enabled          # Window Scaling heuristics
     netsh int tcp set supplemental internet minrto=300                 # Controls TCP retransmission timeout. 20 to 300 msec.
     netsh int tcp set supplemental internet icw=10                     # Controls initial congestion window. 2 to 64 MSS
-    netsh int tcp set supplemental internet congestionprovider=dctcp   # Controls the congestion provider. cubic ctcp dctcp 
-    netsh int tcp set supplemental internet enablecwndrestart=enabled  # Controls whether congestion window is restarted.
+    netsh int tcp set supplemental internet congestionprovider=newreno # Controls the congestion provider. Default: cubic
+    netsh int tcp set supplemental internet enablecwndrestart=disabled # Controls whether congestion window is restarted.
     netsh int tcp set supplemental internet delayedacktimeout=40       # Controls TCP delayed ack timeout. 10 to 600 msec.
     netsh int tcp set supplemental internet delayedackfrequency=2      # Controls TCP delayed ack frequency. 1 to 255.
     netsh int tcp set supplemental internet rack=enabled               # Controls whether RACK time based recovery is enabled.
@@ -168,13 +168,13 @@ if ($do -ne 'reset') {
     netsh int tcp set security profiles=disabled                       # Profiles protection (private vs domain)
 
     netsh int tcp set global rss=enabled                    # Enable receive-side scaling.
-    netsh int tcp set global autotuninglevel=$RWSCALING     # Fix the receive window at its default value  highlyrestricted normal
+    netsh int tcp set global autotuninglevel=$RWSCALING     # Fix the receive window at its default value
     netsh int tcp set global ecncapability=enabled          # Enable/disable ECN Capability.
     netsh int tcp set global timestamps=enabled             # Enable/disable RFC 1323 timestamps.
-    netsh int tcp set global initialrto=1000                # Connect (SYN) retransmit time (in ms). default: 1000.
+    netsh int tcp set global initialrto=1000                # Connect (SYN) retransmit time (in ms).
     netsh int tcp set global rsc=disabled                   # Enable/disable receive segment coalescing.
-    netsh int tcp set global nonsackrttresiliency=disabled  # Enable/disable rtt resiliency for non sack clients. default: disabled
-    netsh int tcp set global maxsynretransmissions=2        # Connect retry attempts using SYN packets. default: 2.
+    netsh int tcp set global nonsackrttresiliency=disabled  # Enable/disable rtt resiliency for non sack clients.
+    netsh int tcp set global maxsynretransmissions=4        # Connect retry attempts using SYN packets.
     netsh int tcp set global fastopen=enabled               # Enable/disable TCP Fast Open.
     netsh int tcp set global fastopenfallback=enabled       # Enable/disable TCP Fast Open fallback.
     netsh int tcp set global hystart=enabled                # Enable/disable the HyStart slow start algorithm.
@@ -183,8 +183,9 @@ if ($do -ne 'reset') {
 
     netsh int ip set global loopbacklargemtu=enable         # Loopback Large Mtu
     netsh int ip set global loopbackworkercount=4           # Loopback Worker Count 1 2 4
-    netsh int ip set global loopbackexecutionmode=adaptive  # Loopback Execution Mode adaptive|inline|worker
-    netsh int ip set global reassemblylimit=267748640       # Reassembly Limit 267748640|100238784|94371840|0
+    netsh int ip set global loopbackexecutionmode=inline    # Loopback Execution Mode adaptive|inline|worker
+    netsh int ip set global reassemblylimit=267748640       # Reassembly Limit 267748640|0
+    netsh int ip set global reassemblyoutoforderlimit=48    # Reassembly Out Of Order Limit 32
     netsh int ip set global sourceroutingbehavior=drop      # Source Routing Behavior drop|dontforward
     netsh int ip set dynamicport tcp start=32769 num=32766  # DynamicPortRange tcp
     netsh int ip set dynamicport udp start=32769 num=32766  # DynamicPortRange udp
@@ -195,6 +196,7 @@ if ($do -ne 'reset') {
 
 if ($do -eq 'reset') {
   write-host " Reset" -fore Yellow
+
   " Reset SG TCPOptimizer tweaks"; . {
     $NIC.Values |foreach {
       rp "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\$_" TcpAckFrequency -force -ea 0
@@ -225,7 +227,7 @@ if ($do -eq 'reset') {
     rp "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" DisableTaskOffload -force -ea 0
     rp "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" MaximumReassemblyHeaders -force -ea 0  # 0
   } 2>'' 1>''
-
+  
   " Temporarily disable $NICs"; . { $NIC.Keys | foreach { Disable-NetAdapter -InterfaceAlias "$_" -Confirm:$False } }
 
   " Reset-NetAdapterAdvancedProperty"; . { $NIC.Keys |foreach {
@@ -237,8 +239,10 @@ if ($do -eq 'reset') {
   " Re-enable $NICs"; . { $NIC.Keys | foreach { Enable-NetAdapter -InterfaceAlias "$_" -Confirm:$False } }
 
   " Reset netsh"; . {
-    netsh int ip reset; netsh int ipv6 reset; netsh int ipv4 reset; netsh int tcp reset; netsh int udp reset; netsh winsock reset
     netsh int ip set dynamicport tcp start=49152 num=16384; netsh int ip set dynamicport udp start=49152 num=16384
+    netsh int ip set global reassemblyoutoforderlimit=32;   netsh int ip set global reassemblylimit=267748640
+    netsh int ip set global loopbackexecutionmode=adaptive; netsh int ip set global sourceroutingbehavior=dontforward
+    netsh int ip reset; netsh int ipv6 reset; netsh int ipv4 reset; netsh int tcp reset; netsh int udp reset; netsh winsock reset
   } 2>'' 1>''
 
   " Reset QoS"; . {
@@ -248,8 +252,8 @@ if ($do -eq 'reset') {
     rp "HKLM:\SOFTWARE\Policies\Microsoft\Windows\QoS" "Tcp Autotuning Level" -force -ea 0
     Get-NetQosPolicy | Remove-NetQosPolicy -Confirm:$False -ea 0
   } 2>'' 1>''
-  
-  . { gpupdate /force } 2>'' 1>''
+
+  # . { gpupdate /force } 2>'' 1>''
 }
 
 #:: show network configuration
